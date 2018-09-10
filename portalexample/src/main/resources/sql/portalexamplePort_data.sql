7597fc2db2f0f134365dc36506818433c4498a2a
Eugenio Santoboni <eugenio.santoboni@gmail.com>
2018-09-10 10:00:29 +0200

    EN-1937 Project upgrade

--- portalexample/src/main/resources/sql/portalexamplePort_data.sql
+++ portalexample/src/main/resources/sql/portalexamplePort_data.sql
@@ -1830,7 +1830,7 @@ INSERT INTO pages (code, parentcode, pos) VALUES ('resolutions', 'informations',
 INSERT INTO pages (code, parentcode, pos) VALUES ('news', 'informations', 3);
 INSERT INTO pages (code, parentcode, pos) VALUES ('search_result', 'service', 4);
 INSERT INTO pages (code, parentcode, pos) VALUES ('announcements_read', 'announcements', 1);
-INSERT INTO pages (code, parentcode, pos) VALUES ('resolutions_read', 'resolutions', 1,);
+INSERT INTO pages (code, parentcode, pos) VALUES ('resolutions_read', 'resolutions', 1);
 INSERT INTO pages (code, parentcode, pos) VALUES ('news_read', 'news', 1);
 INSERT INTO pages (code, parentcode, pos) VALUES ('contents_read', 'service', 5);
 INSERT INTO pages (code, parentcode, pos) VALUES ('mayor', 'municipality', 1);
@@ -1841,7 +1841,7 @@ INSERT INTO pages (code, parentcode, pos) VALUES ('documents', 'online_services'
 INSERT INTO pages (code, parentcode, pos) VALUES ('download', 'documents', 1);
 INSERT INTO pages (code, parentcode, pos) VALUES ('self_certifications', 'online_services', 2);
 INSERT INTO pages (code, parentcode, pos) VALUES ('old_town_traffic_pass', 'online_services', 3);
-INSERT INTO pages (code, parentcode, pos) VALUES ('old_town_traffic_pass_intservl', 'old_town_traffic_pass', 1,);
+INSERT INTO pages (code, parentcode, pos) VALUES ('old_town_traffic_pass_intservl', 'old_town_traffic_pass', 1);
 INSERT INTO pages (code, parentcode, pos) VALUES ('public_api', 'online_services', 4);
 INSERT INTO pages (code, parentcode, pos) VALUES ('informations', 'homepage', 3);
 
